d796f285d3e33b254bc324361a513334e0826827
sizweludick <ludicksizwe1@gmail.com>
2022-01-23 10:51:54 +0200

    Updated services

--- app/c_create_post/service.py
+++ app/c_create_post/service.py
@@ -1,6 +1,8 @@
+import imp
 from app.x_db_models import session, User, Post
 from app.c_create_post.input import CreatePostModel
-
+from fastapi.responses import JSONResponse
+from fastapi import HTTPException
 
 class CreatePost:
 
@@ -10,7 +12,7 @@ class CreatePost:
     def create_post(self):
         # check if the user exists
         try:
-            user = session.query(User).filter_by(id = self.__inputs.user_id).one()
+            user = Userfilter_by(id = self.__inputs.user_id).one()
             if user :
                 post = Post(
                     user_id=self.__inputs.user_id,
@@ -20,8 +22,8 @@ class CreatePost:
 
                 session.add(post)
                 session.commit()
-                return {"post_id":post.id , "heading":post.heading,"body":post.body}
+                return JSONResponse(status_code=201, content ={"post_id":post.id , "heading":post.heading,"body":post.body})
 
         except Exception as error:
             session.rollback()
-            return {"status":"failed", "message": "invalid post creation"}
+            return HTTPException(status_code= 401, detail ="invalid post creation")
